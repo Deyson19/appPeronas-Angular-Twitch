ec74e2242b302f0fe97c5fcd88d73b22b51efae7
Deyson Venté <deiandresvent@hotmail.com>
2024-11-12 16:05:23 -0500

    rutas principales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: UsuariosComponent,
+  },
+  {
+    path: 'usuario/:id',
+    loadComponent: () => import('./components/usuario/usuario.component'),
+  },
+];
